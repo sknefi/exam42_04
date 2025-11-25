0c5c72fae76016a1ff6324a5b9422994a225e1e5
sknefi <filip.karika1@gmail.com>
2025-11-25 13:04:31 +0100

    reseted given.c in urtest, added .gitignore

--- ur_tests/given.c
+++ ur_tests/given.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <malloc.h> // change this to <stdlib.h>
+#include <malloc.h>
 #include <ctype.h>
 
 typedef struct node {
@@ -13,7 +13,7 @@ typedef struct node {
     struct node *r;
 }   node;
 
-node *n(**s); // delete this guy
+node *n(**s);
 
 node    *new_node(node n)
 {
@@ -41,12 +41,12 @@ void    unexpected(char c)
     if (c)
         printf("Unexpected token '%c'\n", c);
     else
-        printf("Unexpected end of file\n"); // printf("Unexpected end of input\n");
+        printf("Unexpected end of file\n");
 }
 
 int accept(char **s, char c)
 {
-    if (**s) // if (**s == c) 
+    if (**s)
     {
         (*s)++;
         return (1);
@@ -62,10 +62,8 @@ int expect(char **s, char c)
     return (0);
 }
 
-node    *parse_expr(char *s) // delete whole function
+node    *parse_expr(char *s)
 {
-    //...
-
     if (*s) 
     {
         destroy_tree(ret);
@@ -73,129 +71,6 @@ node    *parse_expr(char *s) // delete whole function
     }
     return (ret);
 }
-
-/******************************************************************************
-ADD THESE FUNCTIONS BELOW
-******************************************************************************/
-
-/*
-int check_balance(char *s)
-{
-    int balance = 0;
-    while (*s)
-    {
-        if (*s == '(')
-            balance++;
-        else if (*s == ')')
-        {
-            balance--;
-            if (balance < 0)
-                return (-1);
-        }
-        s++;
-    }
-    return (balance);
-}
-*/
-
-/*
-node *parse_number_or_group(char **s)
-{
-    node *res;
-    node tmp;
-
-    res = NULL;
-
-    if (**s == '(')
-    {
-        (*s)++;
-        res = parse_addition(s);
-        if (!res || **s != ')')
-        {
-            destroy_tree(res);
-            unexpected(**s);
-            return (NULL);
-        }
-        (*s)++;
-        return (res);
-    }
-
-    if (isdigit(**s))
-    {
-        tmp.type = VAL;
-        tmp.val = **s - '0';
-        res = new_node(tmp);
-        (*s)++;
-        return (res);
-    }
-
-    unexpected(**s);
-    return (NULL);
-}
-*/
-
-/*
-node *parse_multiplication(char **s)
-{
-    node *left;
-    node *right;
-    node tmp;
-
-    left = parse_number_or_group(s);
-    if (!left)
-        return (NULL);
-
-    while (**s == '*')
-    {
-        (*s)++;
-        right = parse_number_or_group(s);
-        if (!right)
-        {
-            destroy_tree(left);
-            return (NULL);
-        }
-        tmp.type = MULTI;
-        tmp.l = left;
-        tmp.r = right;
-        left = new_node(tmp);
-    }
-    return (left);
-}
-*/
-
-/*
-node *parse_addition(char **s)
-{
-    node *left;
-    node *right;
-    node tmp;
-
-    left = parse_multiplication(s);
-    if (!left)
-        return (NULL);
-
-    while (**s == '+')
-    {
-        (*s)++;
-        right = parse_multiplication(s);
-        if (!right)
-        {
-            destroy_tree(left);
-            return (NULL);
-        }
-        tmp.type = ADD;
-        tmp.l = left;
-        tmp.r = right;
-        left = new_node(tmp);
-    }
-    return (left);
-}
-*/
-
-/******************************************************************************
-END OF FUNCTIONS YOU MUST ADD
-******************************************************************************/
-
 
 int eval_tree(node *tree)
 {
@@ -215,12 +90,7 @@ int main(int argc, char **argv)
     if (argc != 2)
         return (1);
 
-    /*
-    if (check_balance(argv[1]) == -1)
-        return(printf("Unexpected token ')'"), 1);
-    */
-
-    node *tree = parse_expr(argv[1]); // node *tree = parse_addition(&argv[1]);
+    node *tree = parse_expr(argv[1]);
 
     if (!tree)
         return (1);
